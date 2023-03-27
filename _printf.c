0d9f651b68df62a3834a61f94ce3f66ee5509949
Winnie Muthoni <winniemsoo58@gmail.com>
2023-03-27 07:06:15 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,112 +1,127 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+/**
+* no_struct - a helper function that is called when there is a %something
+* @c: the character passed that was after %
+* @count: the number of count thus far. it will be incremented
+* @argu: the va_list that is passed to us so we can va_arg it
+*
+* Description: NO I WILL NOT USE STRUCTS LIKE EVERYONE ELSE
+* Return: the count total
+*
+* A: we passed that character after the %. use it as switch condition
+* B: if it is a char, we put char and plus 1 count
+* C: if it is a string we check if null, if so then we put null and plus 6
+*	we also put string if not null and count the stuff
+* D: if it is an i, do the same as a d. so we put i and let it cascade down
+*	if the number is 0 then we add 1 to count and put a 0
+*	else we just print the number
+* E: if it was a % then we just plus 1 and put the %
+* F: Binary. havent got it to work yet. it should.
+* G: if it is r, call rev_str function to print string in reverse.
+* H: the default is to just print the %letter and yea...
+*/
+
+int no_struct(char c, int count, va_list argu)
+{
+	int j;
+	char *s;
+
+	switch (c)/* A */
+	{
+		case 'c':/* B */
+			j = va_arg(argu, int);
+			count += _putchar(j);
+			break;
+		case 's':/* C */
+			s = va_arg(argu, char *);
+			if (!s)
+			{
+				_putchar('(');
+				_putchar('n');
+				_putchar('u');
+				_putchar('l');
+				_putchar('l');
+				_putchar(')');
+				count += 6;
+			}
+			else
+				count += _putstring(s);
+			break;
+		case 'i':/* D */
+		case 'd':
+			j = va_arg(argu, int);
+			if (!j)
+			{
+				count++;
+				_putchar('0');
+			}
+			else
+				count += print_number(j);
+			break;
+		case '%':/* E */
+			count += _putchar('%');
+			break;
+		case 'b':/* F */
+			j = va_arg(argu, int);
+			count += dec_to_binary(j);
+			break;
+		case 'r': /* G */
+			s = va_arg(argu, char *);
+			count += rev_str(s);
+			break;
+		case 'R': /* H */
+			s = va_arg(argu, char *);
+			count += rot13(s);
+			break;
+		default:/* H */
+			count += 2;
+			_putchar('%');
+			_putchar(c);
+	}
+	return (count);
+}
 
 /**
-
-- _printf - Printf function
-- @format: format.
-- Patrick and Winnie team project
-- Return: Printed chars.
-- /
+* _printf - our own printf function
+* @format: A character string, composed of zero of more directives
+*
+* Description: Writes a formatted string to the standard output
+* Return: an integer. The number of characters printed excluding the null byte
+* A: if format is null then we return -1
+* B: as long as format of index is not null, we increment
+* C: if the index is not a percentage then we puts and count++
+* D: ERASED THE DEEEE
+* E: if it is not a null then we scan that letter. pass it into helper func
+* F: its prob a null so we return -1
+*/
 
 int _printf(const char *format, ...)
-
 {
+	int i = 0;
+	int count = 0;
+	va_list argu;
 
-int i, printed = 0, printed_chars = 0;
+	va_start(argu, format);
 
-int flags, width, precision, size, buff_ind = 0;
+	if (!format)/* A */
+		return (-1);
 
-va_list list;
-
-char buffer[BUFF_SIZE];
-
-if (format == NULL)
-
-return (-1);
-
-va_start(list, format);
-
-for (i = 0; format && format[i] != '\0'; i++)
-
-{
-
-if (format[i] != '%')
-
-{
-
-buffer[buff_ind++] = format[i];
-
-if (buff_ind == BUFF_SIZE)
-
-print_buffer(buffer, &buff_ind);
-
-/* write(1, &format[i], 1);*/
-
-printed_chars++;
-
+	for (i = 0; format[i]; i++)/* B */
+	{
+		if (format[i] != '%')/* C */
+		{
+			count++;
+			_putchar(format[i]);
+		}
+		else if (format[i + 1])/* E */
+		{
+			i++;
+			count = no_struct(format[i], count, argu);
+		}
+		else/* F */
+			return (-1);
+	}
+	va_end(argu);
+	return (count);
 }
-
-else
-
-{
-
-print_buffer(buffer, &buff_ind);
-
-flags = get_flags(format, &i);
-
-width = get_width(format, &i, list);
-
-precision = get_precision(format, &i, list);
-
-size = get_size(format, &i);
-
-++i;
-
-printed = handle_print(format, &i, list, buffer,
-
-flags, width, precision, size);
-
-if (printed == -1)
-
-return (-1);
-
-printed_chars += printed;
-
-}
-
-}
-
-print_buffer(buffer, &buff_ind);
-
-va_end(list);
-
-return (printed_chars);
-
-}
-
-/**
-
-- print_buffer - Prints the contents of the buffer if it exist
-- @buffer: Array of chars
-- @buff_ind: Index at which to add next char, represents the length.
-- Patrick and Winnie team project
-- /
-
-void print_buffer(char buffer[], int *buff_ind)
-
-{
-
-if (*buff_ind > 0)
-
-write(1, &buffer[0], *buff_ind);
-
-- buff_ind = 0;
-
-}
-
-
-
-
-
